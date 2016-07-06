c5445a167ce085b1432226c84b7a488d126dd292
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:38:32 -0700

    Intro and first guess

--- game.py
+++ game.py
@@ -1,1 +1,5 @@
+print "Hello, welcome to our game!"
 name = raw_input("Hello, what is your name? ")
+print "Hello %s, I'm thinking of a number between 1 and 100" % (name)
+print "Try to guess my number."
+guess = int(raw_input("What's your guess? "))
